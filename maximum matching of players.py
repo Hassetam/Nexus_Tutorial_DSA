d9670a3dfc29e8a4e10a11ade2cb6b145c960203
Hassetam <adugnahasset@gmail.com>
2025-10-24 19:45:39 +0300

    Implement maximum matching of players and trainers

---
+++ maximum matching of players.py
@@ -0,0 +1,16 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        i,j = len(players)-1 , len(trainers)-1
+        count = 0
+
+        while i >= 0 and j >= 0:
+            if players[i] <= trainers[j]:
+                count += 1
+                i -= 1
+                j -= 1
+            else:
+                i -= 1
+
+        return count
